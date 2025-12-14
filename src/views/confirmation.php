01bba73599fc6c02ac9d000ee5b08260b82e991a
takeshi-nakamura06 <codebug0527@gmail.com>
2025-12-14 22:46:26 +0900

    update readme.md
    Co-authored-by:  stardev00012
    <stardev00012@gmail.com>

--- src/views/confirmation.php
+++ src/views/confirmation.php
@@ -17,8 +17,8 @@ require __DIR__ . '/header.php';
       <div class="col-md-6 col-md-offset-3">
         <div class="block text-center">
         	<i class="tf-ion-android-checkmark-circle"></i>
-          <h2 class="text-center">Thank you for shopping with us</h2>
-          <a href="/products" class="btn btn-main mt-20">Continue Shopping</a>
+          <h2 class="text-center">Thanksou for shopping with us</h2>
+          <a href="/products" class="btn btn-main mt-20">Continue...</a>
         </div>
       </div>
     </div>
